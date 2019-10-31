c9cd04d300c48a27f4b7d9e063ca99d92f522dd4
rajaramthumati <35828671+rajaramthumati@users.noreply.github.com>
2019-10-31 16:09:51 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 mvn clean install sonar:sonar
 sleep 5
 cp $1/target/hackthonCndHyd.war $1/src/resources/docker/hackthonCndHyd.war
-mvn docker:build -DpushImageTag -DdockerImageTags=$2
+mvn docker:build -DskipDockerPush -DdockerImageTags=$2
 #-DskipDockerTag -DskipDockerPush
