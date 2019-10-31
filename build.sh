dd53ccc534f098bd2f8b269c11486b9536e28df5
rajaramthumati <35828671+rajaramthumati@users.noreply.github.com>
2019-10-31 14:08:01 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 mvn clean install
 sleep 5
 cp $1/target/hackthonCndHyd.war $1/src/resources/docker/hackthonCndHyd.war
-mvn docker:build -DskipDockerTag
+mvn docker:build -DskipDockerTag -DskipDockerPush
